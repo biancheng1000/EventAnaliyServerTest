b31bc2b39ae7d40fe9c3ac1469afa95e9dcc4765
biancheng1000 <drogensection@hotmail.com>
2016-10-15 17:05:33 +0800

    server

---
+++ EventAnaliyServiceEmp.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TennisDataAnaliy;
+
+namespace DataProviderService
+{
+    public class EventAnaliyServiceEmp: TennisDataAnaliy.TennisDataAnaliy.Iface
+    {
+
+        #region Iface
+        /// <summary>
+        /// 接收来自底层的数据
+        /// </summary>
+        /// <param name="path"></param>
+        public void InsertPathToDB(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 接收来自界面传递来的用户信息
+        /// </summary>
+        /// <param name="useinfo"></param>
+        public void SetPlayers(List<userInfo> useinfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 接收来自客户端当前的模式
+        /// 比赛 训练（需要目标区域号码）
+        /// </summary>
+        /// <param name="model"></param>
+        public void SetRunningModel(RunningMode model)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region 接收到底层坐标信息进行事件分析
+        /// <summary>
+        /// 匹配人员坐标
+        /// </summary>
+        /// <param name="SubjectString"></param>
+        private void MatchPerson(string SubjectString)
+        {
+            string ResultString = null;
+            try
+            {
+                ResultString = Regex.Match(SubjectString, "[0]\\|[0-9]*\\|[0-9]*\\.[0-9]*,[0-9]*\\.[0-9]*,[0-9]*\\.[0-9]*").Value;
+            }
+            catch (ArgumentException ex)
+            {
+                // Syntax error in the regular expression
+            }
+        }
+        /// <summary>
+        /// 匹配数据
+        /// </summary>
+        /// <param name="SubjectString"></param>
+        private void matchSpeed(string SubjectString)
+        {
+            string ResultString = null;
+            try
+            {
+                ResultString = Regex.Match(SubjectString, "[2]\\|[0-9]*\\.[0-9]*").Value;
+            }
+            catch (ArgumentException ex)
+            {
+                // Syntax error in the regular expression
+            }
+        }
+
+        /// <summary>
+        /// 匹配轨迹数据
+        /// </summary>
+        /// <param name="subjectstring"></param>
+        private void MatchTracks(string subjectstring)
+        {
+            string ResultString = null;
+            try
+            {
+                MatchCollection results = Regex.Matches(subjectstring, "[0-9]*,[0-9]*\\.[0-9]*,[0-9]*\\.[0-9]*,[0-9]*\\.[0-9]*,[0-9]*\\.[0-9]*(;¦|)");
+            }
+            catch (ArgumentException ex)
+            {
+                
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Transport;
+using System.IO;
+namespace EventAnaliyServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TSocket socket = new TSocket("127.0.0.1", 1899);
+            TBinaryProtocol pro = new TBinaryProtocol(socket);
+            TennisDataAnaliy.TennisDataAnaliy.Client client = new TennisDataAnaliy.TennisDataAnaliy.Client(pro);
+            socket.Open();
+            Console.WriteLine("可以开始测试了");
+            TennisDataAnaliy.userInfo user = new TennisDataAnaliy.userInfo();
+            user.UserID = 1;
+           // user.Rect = "(0,0),(100,100)";
+            client.UpdatePlayers(new List<TennisDataAnaliy.userInfo>() { user});
+            Console.WriteLine("用户信息设置完成");
+            TennisDataAnaliy.RunningMode model = new TennisDataAnaliy.RunningMode();
+            model.Model = "train";
+            model.TargetMarkNumber = "8";
+            client.SetRunningModel(model);
+            Console.WriteLine("工作模式设置完成");
+            string path = @"E:\网球\03网球鹰眼顶级赛事版\02doc\00-项目文档\Log.txt";
+            StreamReader reader = new StreamReader(path);
+            while (!reader.EndOfStream)
+            {
+               string trace= reader.ReadLine();
+                client.InsertPathToDB(trace);
+            }
+            Console.WriteLine("轨迹数据发送完毕");
+        }
+    }
+}
